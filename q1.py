fcb24314590fada80c1592e9c9e3b12fb3f2c226
yuankui <kui.yuan@fraudmetrix.cn>
2015-06-08 10:10:13 +0800

    update

---
+++ q1.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer[]}
+    def twoSum(self, nums, target):
+        exist_hash = {}
+        for index, num in enumerate(nums):
+            exist_hash[num] =  index
+
+        for index, left_num in enumerate(nums):
+            right_num = target - left_num
+            right_index = exist_hash.get(right_num)
+            if right_index == None:
+                continue
+
+            if right_index > index:
+                return [index + 1, right_index + 1]
