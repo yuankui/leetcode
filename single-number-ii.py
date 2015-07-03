5f9f4cf4473c92732d8f64c0cad2387420ffc195
yuankui <kui.yuan@fraudmetrix.cn>
2015-07-03 09:10:08 +0800

    update

---
+++ single-number-ii.py
@@ -0,0 +1,25 @@
+import unittest
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def singleNumber(self, nums):
+        hash = {}
+        for num in nums:
+            if num in hash:
+                hash[num] += 1
+            else:
+                hash[num] = 1
+
+
+        for k, v in hash.items():
+            if v != 3:
+                return k
+
+
+class SolutionTest(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def testGenerateList(self):
+        pass
