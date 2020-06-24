b5093838d561258b38269f62f3f00cb5e7065039
yuankui <yuankui@meituan.com>
2020-06-24 17:08:31 +0800

    add leetcode-java

---
+++ leetcode-java/src/main/kotlin/com/yuankui/leetcode/LeetcodeApplication.kt
@@ -0,0 +1,11 @@
+package com.yuankui.leetcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeetcodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<LeetcodeApplication>(*args)
+}
