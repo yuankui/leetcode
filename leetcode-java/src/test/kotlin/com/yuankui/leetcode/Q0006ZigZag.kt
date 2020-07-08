440cca68331724260e46d0f14dbd1f01ba86fdcc
yuankui <yuankui64@gmail.com>
2020-07-08 23:33:35 +0800

    up

--- leetcode-java/src/test/kotlin/com/yuankui/leetcode/Q0006ZigZag.kt
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/Q0006ZigZag.kt
@@ -1,7 +1,68 @@
 package com.yuankui.leetcode
 
+import org.junit.jupiter.api.Test
+
 class Q0006ZigZag {
     fun convert(s: String, numRows: Int): String {
-        return ""
+        val cube = initCharCube(numRows, s.length)
+
+        s.forEachIndexed { i: Int, c: Char ->
+            val w = numRows + numRows - 2
+
+            val base = i / w
+            val remain = i % w
+
+
+
+            if (remain < numRows) {
+                cube[remain][base * (numRows - 1)] = c
+            } else {
+                cube[(numRows-1)- (remain - numRows + 1)][base * (numRows - 1) + numRows - (w - remain) - 1] = c
+            }
+
+            for (arrayOfChars in cube) {
+                println(arrayOfChars.map {
+                    if (it == null)
+                        "_" else it
+                }.joinToString(""))
+            }
+            println()
+        }
+
+        val sb = StringBuffer()
+
+        for (row in 0 until numRows) {
+            for (column in 0 until s.length) {
+                val c = cube[row][column]
+                if (c != null) {
+                    sb.append(c)
+                }
+            }
+        }
+
+
+        return sb.toString()
+    }
+
+    fun initCharCube(m: Int, n: Int): Array<Array<Char?>> {
+        return Array<Array<Char?>>(m, init = {
+            Array(n, init = {
+                null
+            })
+        })
+    }
+
+    @Test
+    fun test1() {
+        val a = Q0006ZigZag().convert("PAYPALISHIRING", 3)
+
+        println("a = $a")
+    }
+
+    @Test
+    fun test2() {
+        val a = Q0006ZigZag().convert("PAYPALISHIRING", 4)
+
+        println("a = $a")
     }
 }
