9f126a5cd22bb37f03d40b29aa6a7a4cc0c57fad
yuankui <yuankui64@gmail.com>
2020-12-01 23:17:00 +0800

    q16

---
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/q16/Solution.kt
@@ -0,0 +1,50 @@
+package com.yuankui.leetcode.q16
+
+import org.junit.jupiter.api.Test
+
+class Solution {
+    fun threeSumClosest(nums: IntArray, target: Int): Int {
+
+        nums.sort()
+
+        var min = Int.MAX_VALUE
+        var sum = 0
+        
+        for (i in nums.indices) {
+            val a= nums[i]
+
+            // pointer
+            var pLeft = i + 1
+            var pRight = nums.size - 1
+
+            while (pLeft < pRight) {
+                val left = nums[pLeft]
+                val right = nums[pRight]
+                val delta = Math.abs(left + right + a - target)
+                if (delta < min) {
+                    min = delta
+                    sum = left + right + a
+                }
+                
+                if (left + right + a - target == 0) {
+                    return left + right + a
+                } else if (left + right + a - target < 0) {
+                    pLeft += 1
+                } else {
+                    pRight -= 1
+                }
+            }
+        }
+        return sum
+    }
+    
+    
+    @Test
+    fun test() {
+        val array = intArrayOf(0, 1, 2)
+        val target = 3
+        val threeSumClosest = this.threeSumClosest(array, target)
+
+        println("threeSumClosest = ${threeSumClosest}")
+    }
+}
