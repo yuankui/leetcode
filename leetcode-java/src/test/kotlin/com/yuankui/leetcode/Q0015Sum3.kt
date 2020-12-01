fc64f5085d97f8945bdee2465d283852641cc94e
yuankui <yuankui64@gmail.com>
2020-12-01 22:53:46 +0800

    up

--- leetcode-java/src/test/kotlin/com/yuankui/leetcode/Q0015Sum3.kt
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/Q0015Sum3.kt
@@ -4,41 +4,48 @@ import org.junit.jupiter.api.Test
 
 class Q0015Sum3 {
     fun threeSum(nums: IntArray): List<List<Int>> {
+        if (nums.size < 3) {
+            return listOf()
+        }
+        
         nums.sort()
-
-        val map: MutableMap<Int, MutableSet<List<Int>>> = mutableMapOf()
 
         val solution: MutableSet<List<Int>> = mutableSetOf()
         
-        for (i in 2 until nums.size) {
-            // 将当前元素和签名所有元素组合，将组合加入到map中
-            for (j in 0 until (i-2)) {
-                val a = nums[i-1]
-                val b = nums[j]
-                val sum = a + b
-                val list = map.getOrDefault(sum, mutableSetOf())
-                list.add(listOf(a, b))
-                map[sum] = list
-            }
-
-            val c = nums[i]
-            val list = map[-c]
-            if (list != null) {
-                list.map { 
-                    listOf(it[0], it[1], c)
-                }.forEach {
-                    solution.add(it)
+        for (i in nums.indices) {
+            val a= nums[i]
+            
+            // pointer
+            var pLeft = i + 1
+            var pRight = nums.size - 1
+            
+            while (pLeft < pRight) {
+                val left = nums[pLeft]
+                val right = nums[pRight]
+                if (left + right + a == 0) {
+                    solution.add(listOf(a, left, right))
+                    if (left == nums[pLeft + 1]) {
+                        pLeft += 1
+                    } else if (right == nums[pRight - 1]) {
+                        pRight -= 1
+                    } else {
+                        pLeft += 1
+                        pRight -= 1
+                    }
+                } else if (left + right + a < 0) {
+                    pLeft += 1
+                } else {
+                    pRight -= 1
                 }
             }
         }
-        
         return solution.toList()
     }
     
     @Test
     fun test1() {
         val run = Q0015Sum3()
-        val res = run.threeSum(intArrayOf(-1,0,1,2,-1,-4))
+        val res = run.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))
         
         println("res = $res")
 
