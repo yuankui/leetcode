5deae39d55e7ad33401762ce02a32f4ec77d5e2b
yuankui <yuankui64@gmail.com>
2020-12-01 22:22:34 +0800

    slow

--- leetcode-java/src/test/kotlin/com/yuankui/leetcode/Q0015Sum3.kt
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/Q0015Sum3.kt
@@ -4,47 +4,42 @@ import org.junit.jupiter.api.Test
 
 class Q0015Sum3 {
     fun threeSum(nums: IntArray): List<List<Int>> {
-        // value -> list(index)
-        val map = mutableMapOf<Int, MutableList<Int>>()
+        nums.sort()
 
-        val groups = nums.groupBy { it }
-                .mapValues { it.value.size }
+        val map: MutableMap<Int, MutableSet<List<Int>>> = mutableMapOf()
+
+        val solution: MutableSet<List<Int>> = mutableSetOf()
         
-        groups.entries.forEachIndexed {
-            index: Int, entry: Map.Entry<Int, Int> -> 
-            val value = entry.key
-            val count = entry.value
-        }
-        nums.forEachIndexed { index: Int, num: Int ->
-            val list = map.get(num)
-            if (list == null) {
-                map.put(num, mutableListOf(index))
-            } else {
-                list.add(index)
+        for (i in 2 until nums.size) {
+            // 将当前元素和签名所有元素组合，将组合加入到map中
+            for (j in 0 until (i-2)) {
+                val a = nums[i-1]
+                val b = nums[j]
+                val sum = a + b
+                val list = map.getOrDefault(sum, mutableSetOf())
+                list.add(listOf(a, b))
+                map[sum] = list
             }
-        }
 
-        val res = mutableSetOf<List<Int>>()
-        
-        for (i in nums.indices) {
-            for (j in i + 1 until nums.size) {
-                val target = -(nums[i] + nums[j])
-                val list = map.get(target) ?: continue
-                list.forEach { 
-                    if (it > i && it > j) {
-                        res.add(listOf(nums[i], nums[j], nums[it]).sorted())
-                    }
+            val c = nums[i]
+            val list = map[-c]
+            if (list != null) {
+                list.map { 
+                    listOf(it[0], it[1], c)
+                }.forEach {
+                    solution.add(it)
                 }
             }
         }
         
-        return res.toList()
+        return solution.toList()
     }
     
     @Test
     fun test1() {
         val run = Q0015Sum3()
-        val res = run.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))
+        val res = run.threeSum(intArrayOf(-1,0,1,2,-1,-4))
+        
         println("res = $res")
 
     }
