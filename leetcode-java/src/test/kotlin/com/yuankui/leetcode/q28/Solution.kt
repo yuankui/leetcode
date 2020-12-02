93ca4ab67397119d7589061fbeeab5c06e1a89d3
yuankui <yuankui64@gmail.com>
2020-12-02 22:53:04 +0800

    add

--- leetcode-java/src/test/kotlin/com/yuankui/leetcode/q28/Solution.kt
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/q28/Solution.kt
@@ -1,7 +1,35 @@
 package com.yuankui.leetcode.q28
 
+import org.junit.jupiter.api.Test
+
 class Solution {
     fun strStr(haystack: String, needle: String): Int {
-        return 1
+        if (needle.isEmpty()) {
+            return 0
+        }
+        val max = haystack.length - needle.length
+        outer@ for (i in 0..max) {
+            if (haystack.get(i) == needle.first()) {
+                // 开始逐个比较
+                for (j in needle.indices) {
+                    if (haystack.get(i + j) != needle.get(j)) {
+                        continue@outer
+                    }
+                }
+                return i
+            }
+        }
+        
+        return -1
+    }
+    
+    @Test
+    fun test() {
+        val strStr = this.strStr("hello", "ll")
+        println("strStr = ${strStr}")
+
+        println("this.strStr(\"aaaaa\", \"bba\") = ${this.strStr("aaaaa", "bba")}")
+        
+
     }
 }
---
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/q30/Solution.kt
@@ -0,0 +1,7 @@
+package com.yuankui.leetcode.q30
+
+class Solution {
+    fun findSubstring(s: String, words: Array<String>): List<Int> {
+        return listOf()
+    }
+}
