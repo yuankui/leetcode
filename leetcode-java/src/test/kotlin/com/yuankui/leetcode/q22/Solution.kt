a7626aece7224b813722a7608ce1f3d1f6b2ed37
yuankui <yuankui@meituan.com>
2020-12-02 09:44:07 +0800

    todo

---
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/q22/Solution.kt
@@ -0,0 +1,16 @@
+package com.yuankui.leetcode.q22
+
+/**
+ * [
+    "((()))",
+    "(()())",
+    "(())()",
+    "()(())",
+    "()()()"
+   ]
+ */
+class Solution {
+    fun generateParenthesis(n: Int): List<String> {
+
+    }
+}
