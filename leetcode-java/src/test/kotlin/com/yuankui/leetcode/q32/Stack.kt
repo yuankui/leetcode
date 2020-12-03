1967fce33e0f128ac107412f647d4b46f1b12bff
yuankui <yuankui64@gmail.com>
2020-12-03 23:08:31 +0800

    up

--- leetcode-java/src/test/kotlin/com/yuankui/leetcode/q32/Stack.kt
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/q32/Stack.kt
@@ -1,31 +1,43 @@
 package com.yuankui.leetcode.q32
 
 import org.junit.jupiter.api.Test
+import java.util.Stack
 
 class Stack {
     fun longestValidParentheses(s: String): Int {
         var max = 0
+        class State(val index: Int, val prevLen: Int)
+        val stack: Stack<State> = Stack()
+        var prevLen = 0
+        
+        for (i in s.indices) {
+            val c = s[i]
+            if (c == '(') {
+                stack.push(State(i, prevLen))
+                prevLen = 0
+            } else {
+                if (stack.isEmpty()) {
+                    prevLen = 0
+                    continue
+                }
+
+                val state = stack.pop()
+                
+                val len = i - state.index + 1 + state.prevLen
+                prevLen = len
+                
+                if (len > max) {
+                    max = len
+                }
+            }
+        }
 
         return max
     }
 
     @Test
     fun test() {
-        
-        val ret0 = this.longestValidParentheses("(()())")
+        val ret0 = this.longestValidParentheses(")()())()()(")
         println("ret0 = ${ret0}")
-        
-        this.longestValidParentheses("())")
-
-        val ret1 = this.longestValidParentheses("()(())")
-        println("ret1 = ${ret1}")
-
-        val ret = this.longestValidParentheses("(()")
-        println("ret = ${ret}")
-
-        val ret2 = this.longestValidParentheses(")()())")
-        println("ret2 = ${ret2}")
-
-
     }
 }
