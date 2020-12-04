6139d34a86d1b854caf115a2e111527dfdf13db1
yuankui <yuankui@meituan.com>
2020-12-04 15:13:44 +0800

    remove element

---
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/q27/Solution.kt
@@ -0,0 +1,17 @@
+package com.yuankui.leetcode.q27
+
+class Solution {
+    fun removeElement(nums: IntArray, target: Int): Int {
+        var removed = 0
+        for (i in nums.indices) {
+            val value = nums[i]
+            if (value == target) {
+                removed += 1
+            } else {
+                nums[i-removed] = value
+            }
+        }
+
+        return nums.size - removed
+    }
+}
