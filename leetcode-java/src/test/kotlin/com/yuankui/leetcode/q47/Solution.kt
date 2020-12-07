59ea72f3f85aa516edc5f1e648daf8ec8ae9033c
yuankui <yuankui@meituan.com>
2020-12-07 20:33:52 +0800

    up

--- leetcode-java/src/test/kotlin/com/yuankui/leetcode/q47/Solution.kt
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/q47/Solution.kt
@@ -6,29 +6,22 @@ class Solution {
         
         val res = mutableListOf<List<Int>>()
 
-        fun fill(set: MutableSet<Int>, list: MutableList<Int>, max: Int) {
-            if (set.size == nums.size) {
+        val valueCount = nums.groupBy { it }
+                .mapValues { it.value.size }
+                .toMutableMap()
+
+        fun fill(set: MutableSet<Int>, list: MutableList<Int>) {
+            if (list.size == nums.size) {
                 res.add(list.toList())
                 return
             }
 
-            for (i in nums.indices) {
-                val num = nums[i]
-                if (num in set) continue
+            for (v in valueCount.keys) {
 
-                // 插入
-                set.add(num)
-                list.add(num)
-
-                fill(set, list)
-                
-
-                set.remove(num)
-                list.removeAt(list.size - 1)
             }
         }
 
-        fill(mutableSetOf(), mutableListOf(), Int.MIN_VALUE)
+        fill(mutableSetOf(), mutableListOf())
 
         return res
     }
