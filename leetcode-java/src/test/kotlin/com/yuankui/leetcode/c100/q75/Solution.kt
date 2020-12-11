06a0d4d0b629d0dd351a421c338563db2804caae
yuankui <yuankui@meituan.com>
2020-12-11 17:57:26 +0800

    ok q75

--- leetcode-java/src/test/kotlin/com/yuankui/leetcode/c100/q75/Solution.kt
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/c100/q75/Solution.kt
@@ -1,7 +1,41 @@
 package com.yuankui.leetcode.c100.q75
 
+import org.junit.jupiter.api.Test
+
 class Solution {
     fun sortColors(nums: IntArray): Unit {
-        // TODO 双指针
+        var p0 = 0
+        var p2 = nums.size - 1
+
+        var p = 0
+
+        fun swap(i: Int, j: Int) {
+            val tmp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = tmp
+        }
+        while (p <= p2) {
+            val num = nums[p]
+            if (num == 2) {
+                swap(p2, p)
+                p2 -= 1
+            } else if (num == 0 && p != p0) {
+                swap(p0, p)
+                p0 += 1
+            } else {
+                p += 1
+            }
+        }
+    }
+
+    @Test
+    fun test() {
+        val input = intArrayOf(2, 0, 2, 1, 1, 0)
+
+
+
+        this.sortColors(input)
+
+        println("input.toList() = ${input.toList()}")
     }
 }
