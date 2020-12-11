2150ee66474fd5cb1eb6c0ffad65f81dad12f9cf
yuankui <yuankui64@gmail.com>
2020-12-11 20:40:24 +0800

    ok q71

--- leetcode-java/src/test/kotlin/com/yuankui/leetcode/c100/q71/Split.kt
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/c100/q71/Split.kt
@@ -1,7 +1,33 @@
 package com.yuankui.leetcode.c100.q71
 
+import org.junit.jupiter.api.Test
+
 class Split {
     fun simplifyPath(path: String): String {
+        
+        val stack = ArrayDeque<String>()
+        
+        path.split("/").forEach { token ->
+            if (token == "..") {
+                if (stack.isNotEmpty()) {
+                    stack.removeLast()
+                }
+            } else if (token == ".") {
+                // do nothing
+            } else if (token == "") {
+                // do nothing
+            } else {
+                stack.addLast(token)
+            }
+        }
+        
+        return stack.joinToString("/", "/")
+    }
+
+    @Test
+    fun test() {
+        val res = this.simplifyPath("/a//b////c/d//././/..")
+        println("res = ${res}")
         
     }
 }
