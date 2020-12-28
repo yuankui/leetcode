2fd7bad5811caf77209fdc8166ac07e9d68494c6
yuankui <yuankui64@gmail.com>
2020-12-28 19:27:48 +0800

    ok q88

--- leetcode-java/src/test/kotlin/com/yuankui/leetcode/c100/q88/Solution.kt
+++ leetcode-java/src/test/kotlin/com/yuankui/leetcode/c100/q88/Solution.kt
@@ -1,7 +1,27 @@
 package com.yuankui.leetcode.c100.q88
 
+import org.junit.jupiter.api.Test
+
 class Solution {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        var i = m - 1
+        var j = m + n - 1
+        for (idx in nums2.indices.reversed()) {
+            val num = nums2[idx]
+            while (i >= 0 && nums1[i] >= num) {
+                nums1[j] = nums1[i]
+                j -= 1
+                i -= 1
+            }
+            nums1[j] = num
+            j -= 1
+        }
+//        println("nums1.toList() = ${nums1.toList()}")
+    }
 
+    @Test
+    fun test() {
+//        this.merge(intArrayOf(1, 2, 3, 0, 0, 0), 3, intArrayOf(2, 5, 6), 3)
+        this.merge(intArrayOf(0), 0, intArrayOf(1), 1)
     }
 }
